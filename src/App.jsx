5f1ea3e3737765534c1dd4e4e77bc6efbc5e9656
Ratanont Sukapan <ratanont.s@gmail.com>
2024-11-24 03:38:33 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Link } from "react-router";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
+      <Link to="/posts" className="text-blue-400">
+        All post
+      </Link>
+    </>
+  );
 };
 
 export default App;
